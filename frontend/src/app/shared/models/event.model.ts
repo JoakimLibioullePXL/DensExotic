39c628758522837ae6254d2cbf91c6d562c33eaf
JoakimLibioullePXL <Joakim.libioulle@student.pxl.be>
2025-02-27 21:12:50 +0100

    finished-product-just-need-correct-pictures

--- frontend/src/app/shared/models/event.model.ts
+++ frontend/src/app/shared/models/event.model.ts
@@ -11,6 +11,7 @@ export class Event {
   price: number;
   goodies: string;
   image: string;
+  ticketLink: string;
   activities: Activity[]; // Maximaal 3 activiteiten per event
 
   constructor(
@@ -24,6 +25,7 @@ export class Event {
     price: number,
     goodies: string,
     image: string,
+    ticketLink: string,
     activities: Activity[] // Verwacht een array van 3 activiteiten
   ) {
     this.id = id;
@@ -36,6 +38,7 @@ export class Event {
     this.price = price;
     this.goodies = goodies;
     this.image = image;
+    this.ticketLink = ticketLink;
     this.activities = activities.slice(0, 3); // Beperkt tot maximaal 3 activiteiten
   }
 }
