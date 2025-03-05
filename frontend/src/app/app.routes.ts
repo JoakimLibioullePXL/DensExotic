95c8be53262ba712bf3b47b51f3d21c8220919d1
JoakimLibioullePXL <Joakim.libioulle@student.pxl.be>
2025-03-05 14:07:46 +0100

    added-home-component
    to fix the router outlet issue, added home component and set the '' path to the home component.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { EventDetailComponent } from './event-detail/event-detail.component';
 import { EventsListComponent } from './events-list/events-list.component';
+import {HomeComponent} from './home/home.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent},
   { path: 'events', component: EventsListComponent },
   { path: 'event/:id', component: EventDetailComponent }
 ];
